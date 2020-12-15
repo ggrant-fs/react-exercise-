292b5289ee8c28e5bf6320e0f0b5744335359340
Gary Grant <garygrant@garys-air.myfiosgateway.com>
2020-12-14 21:05:17 -0500

    updated application

--- products-app/src/App.js
+++ products-app/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import React from "react";
+import Home from "./Home";
+import Products from "./Products";
+import AddProduct from "./AddProduct";
+import { Route } from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <React.Fragment>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/Product">
+        <Products />
+      </Route>
+      <Route>
+        <AddProduct />
+      </Route>
+    </React.Fragment>
+  );
+};
 
 export default App;
---
+++ products-app/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <h1>ProductsApp</h1>
+            </header>
+            <hr/>
+        </div>
+    );
+};
+
+export default Header;
